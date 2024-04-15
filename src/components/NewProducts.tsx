8d4a6d484e7cc4033be8eed2d1e5f3be7f464bca
ry3en <34361537+ry3en@users.noreply.github.com>
2024-04-14 22:26:38 -0600

    as

--- src/components/NewProducts.tsx
+++ src/components/NewProducts.tsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import ProductCard from './ProductCard';
 import ProductDetailsModal from './ProductDetailsModal';
 
-interface Product{
+interface Product {
     ID_product: string;
     Product_name: string;
     ID_Category: string;
@@ -35,10 +35,10 @@ const NewProducts: React.FC<NewProductsProps> = ({ products, category }) => {
     return (
         <div>
             <div className="container pt-16">
-                <h2 className="font-medium text-2xl pb-5">
+                <h2 className="text-sm sm:text-lg font-medium pb-5">
                     {category}
                 </h2>
-                <div className="grid grid-cols-1 place-items-center sm:place-items-start sm:grid-cols-2 lg:grid-col-3 xl:grid-cols-4 gap-10 xl:gap-x-20 xl:gap-y-10">
+                <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-2 sm:gap-4">
                     {products.map((item) =>
                         <ProductCard key={item.ID_product} product={item} onProductSelect={showProductDetails} />
                     )}
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -32,25 +32,26 @@ const ProductCard: React.FC<ProductCardProps> = ({
         if (stock > 0) {
             return (
                 <div className="space-y-2 py-2">
-                    <h2 className="text-blackish font-medium uppercase">{product!.Product_name}</h2>
-                    <p> Precio: {product!.PriceSell.replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')} -
+                    <h2 className="text-blackish font-medium truncate">{product!.Product_name}</h2>
+                    <p className="text-xs sm:text-base"> Precio: {product!.PriceSell.replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')} -
                         Talla: {product!.SizeEU}EU </p>
                 </div>
             );
         } else {
             return (
                 <div className="space-y-2 py-2">
-                    <h2 className="text-blackish font-medium uppercase">{product!.Product_name}</h2>
-                    <p> AGOTADO </p>
+                    <h2 className="text-blackish font-medium truncate">{product!.Product_name}</h2>
+                    <p className="text-xs sm:text-base"> AGOTADO </p>
                 </div>
             );
         }
     }
 
     return (
-        <div onClick={handleClick} className="px-4 border border-gray-200  rounded-xl max-w-[400px] cursor-pointer">
+        <div onClick={handleClick} className="px-2 sm:px-4 border border-gray-200 rounded-xl max-w-full cursor-pointer">
             <div>
-                <Image className="w-[3500px] h-[250px]" src={product!.Image_URL} alt={product!.Product_name} width={200} height={300} />
+                <Image className="w-[350px] h-[250px]" src={product.Image_URL} alt={product.Product_name} layout="responsive" width={350} height={250} />
+
             </div>
             {stock(parseInt(product.Quantity))}
         </div>
