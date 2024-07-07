4ccb76f9e57e968bdca008cf7f60ff64ae37089e
kevinbarra <98494916+kevinbarra@users.noreply.github.com>
2024-07-07 16:12:55 -0600

    Update ProductCard.tsx

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,5 +1,7 @@
+// ProductCard.tsx
 import React from "react";
 import Image from "next/image";
+import Link from 'next/link';
 
 interface Product{
     ID_product: string;
@@ -11,13 +13,17 @@ interface Product{
     Revenue: string;
     Color: string;
     Quantity: string;
+
 }
 interface ProductCardProps {
     product: Product;
     onProductSelect: (product: Product) => void;
 }
 
-const ProductCard: React.FC<ProductCardProps> = ({ product, onProductSelect }) => {
+const ProductCard: React.FC<ProductCardProps> = ({
+    product,
+    onProductSelect
+}) => {
     const handleClick = () => {
         onProductSelect(product);
     };
@@ -26,24 +32,25 @@ const ProductCard: React.FC<ProductCardProps> = ({ product, onProductSelect }) =
         if (stock > 0) {
             return (
                 <div className="space-y-2 py-2">
-                    <h2 className="text-blackish font-medium truncate">{product.Product_name}</h2>
-                    <p className="text-sm">Talla: {product.SizeEU}EU</p>
+                    <h2 className="text-blackish font-medium truncate">{product!.Product_name}</h2>
+                    <p className="text-xs sm:text-base">Talla: {product!.SizeEU}EU </p>
                 </div>
             );
         } else {
             return (
                 <div className="space-y-2 py-2">
-                    <h2 className="text-blackish font-medium truncate">{product.Product_name}</h2>
-                    <p className="text-sm"> AGOTADO </p>
+                    <h2 className="text-blackish font-medium truncate">{product!.Product_name}</h2>
+                    <p className="text-xs sm:text-base"> AGOTADO </p>
                 </div>
             );
         }
     }
 
     return (
-        <div onClick={handleClick} className="product_card">
+        <div onClick={handleClick} className="px-2 sm:px-4 border border-gray-200 rounded-xl max-w-full cursor-pointer">
             <div>
-                <Image src={product.Image_URL} alt={product.Product_name} layout="responsive" width={350} height={250} className="rounded-t-xl" />
+                <Image className="w-[350px] h-[250px]" src={product.Image_URL} alt={product.Product_name} layout="responsive" width={350} height={250} />
+
             </div>
             {stock(parseInt(product.Quantity))}
         </div>
