a4bb4d3892aa15d8f663f01acb06f346d27f52f7
kevinbarra <98494916+kevinbarra@users.noreply.github.com>
2024-07-07 15:38:54 -0600

    Update ProductDetailsModal.tsx

--- src/components/ProductDetailsModal.tsx
+++ src/components/ProductDetailsModal.tsx
@@ -22,23 +22,26 @@ interface ProductDetailsModalProps {
 const ProductDetailsModal: React.FC<ProductDetailsModalProps> = ({ product, onClose }) => {
     if (!product) return null;
 
-    function stock(stock: number) {
-        return (
-            <div className="flex">
-                <div className="mr-8">
-                    <Image src={product.Image_URL} alt={product.Product_name} width={400} height={400} />
+    const stock = (stock: number) => {
+        if (product) {
+            return (
+                <div className="flex">
+                    <div className="mr-8">
+                        <Image src={product.Image_URL} alt={product.Product_name} width={400} height={400} />
+                    </div>
+                    <div>
+                        <h2 className="text-2xl font-bold mb-4">{product.Product_name}</h2>
+                        <p>Talla: {product.SizeEU}EU</p>
+                        {stock > 0 ? (
+                            <p className="text-gray-900 text-xl font-semibold">{product.PriceSell} €</p>
+                        ) : (
+                            <h3 className="text-2xl font-bold mb-4">AGOTADO</h3>
+                        )}
+                    </div>
                 </div>
-                <div>
-                    <h2 className="text-2xl font-bold mb-4">{product.Product_name}</h2>
-                    <p>Talla: {product.SizeEU}EU</p>
-                    {stock > 0 ? (
-                        <p className="text-gray-900 text-xl font-semibold">{product.PriceSell} €</p>
-                    ) : (
-                        <h3 className="text-2xl font-bold mb-4">AGOTADO</h3>
-                    )}
-                </div>
-            </div>
-        );
+            );
+        }
+        return null;
     }
 
     return (
