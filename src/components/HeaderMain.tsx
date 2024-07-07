1ba04a7de5e56d31f4f7fc86d1cf455c74cbb9b4
kevinbarra <98494916+kevinbarra@users.noreply.github.com>
2024-07-07 15:13:30 -0600

    Update HeaderMain.tsx

--- src/components/HeaderMain.tsx
+++ src/components/HeaderMain.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import React, { useState, useEffect } from 'react';
-import { BsSearch } from "react-icons/bs";
 import { BsInstagram } from "react-icons/bs";
 import { Fragment } from 'react'
 import { Menu, Transition } from '@headlessui/react'
@@ -40,69 +39,60 @@ const HeaderMain: React.FC<HeaderMainProps> = ({ onCategorySelect }) => {
     }, []);
 
     return (
-        <div className={"border-b border-gray-200 py-2 "}>
-            <div className={"container flex justify-between items-center space-x-2"}>
-                <div className="flex flex-row items-center ">
-                    <div>
-                        <img src="./images/luxaris.png" alt="logo luxaris" className={"w-16 md:w-32 lg:w-48"} />
-                    </div>
-                    <div>
-                        <Menu as="div" className="relative inline-block text-left">
-                            <Menu.Button className="inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-                                Categorias
-                                <ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
-                            </Menu.Button>
-                            <Transition
-                                as={Fragment}
-                                enter="transition ease-out duration-100"
-                                enterFrom="transform opacity-0 scale-95"
-                                enterTo="transform opacity-100 scale-100"
-                                leave="transition ease-in duration-75"
-                                leaveFrom="transform opacity-100 scale-100"
-                                leaveTo="transform opacity-0 scale-95">
-
-                                <Menu.Items className="absolute right-0 z-10 mt-2 w-100 max-h-60 overflow-y-auto origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                                    <Menu.Item >
+        <div className="border-b border-gray-300 py-4">
+            <div className="container mx-auto flex justify-between items-center space-x-4">
+                <div className="flex items-center space-x-4">
+                    <img src="./images/luxaris.png" alt="logo luxaris" className="w-20 md:w-36 lg:w-48" />
+                    <Menu as="div" className="relative inline-block text-left">
+                        <Menu.Button className="inline-flex items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-semibold text-gray-700 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+                            Categorias
+                            <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5 text-gray-500" aria-hidden="true" />
+                        </Menu.Button>
+                        <Transition
+                            as={Fragment}
+                            enter="transition ease-out duration-100"
+                            enterFrom="transform opacity-0 scale-95"
+                            enterTo="transform opacity-100 scale-100"
+                            leave="transition ease-in duration-75"
+                            leaveFrom="transform opacity-100 scale-100"
+                            leaveTo="transform opacity-0 scale-95">
+                            <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                <Menu.Item>
+                                    {({ active }) => (
+                                        <button
+                                            onClick={() => onCategorySelect("ALL", "ALL")}
+                                            className={classNames(
+                                                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                                'block px-4 py-2 text-sm w-full text-left'
+                                            )}>
+                                            ALL
+                                        </button>
+                                    )}
+                                </Menu.Item>
+                                {categories.map((category) => (
+                                    <Menu.Item key={category.ID_Category}>
                                         {({ active }) => (
                                             <button
-                                                onClick={() => onCategorySelect("ALL", "ALL")}
+                                                onClick={() => onCategorySelect(category.ID_Category, category.name)}
                                                 className={classNames(
                                                     active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                                                    'block px-4 py-2 text-sm'
+                                                    'block px-4 py-2 text-sm w-full text-left'
                                                 )}>
-                                                ALL
+                                                {category.name}
                                             </button>
                                         )}
                                     </Menu.Item>
-                                    {categories.map((category) => (
-                                        <Menu.Item key={category.ID_Category}>
-                                            {({ active }) => (
-                                                <button
-                                                    onClick={() => onCategorySelect(category.ID_Category, category.name)}
-                                                    className={classNames(
-                                                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                                                        'block px-4 py-2 text-sm'
-                                                    )}>
-                                                    {category.name}
-                                                </button>
-                                            )}
-                                        </Menu.Item>
-                                    ))}
-                                </Menu.Items>
-                            </Transition>
-                        </Menu>
-                    </div>
+                                ))}
+                            </Menu.Items>
+                        </Transition>
+                    </Menu>
                 </div>
-
-
-                <div className="lg:flex gap-4 text-gray-500 text-[30px] ">
-                <a href="https://www.instagram.com/_luxaris_?igsh=MTdodGhrYWc3djcxcA%3D%3D">
-                        <BsInstagram />
-                    </a>
-                </div>
-
+                <a href="https://www.instagram.com/_luxaris_?igsh=MTdodGhrYWc3djcxcA%3D%3D" className="text-gray-500 text-2xl">
+                    <BsInstagram />
+                </a>
             </div>
         </div>
     )
 };
 export default HeaderMain;
+
