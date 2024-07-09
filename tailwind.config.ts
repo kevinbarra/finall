9d9699a50859a9ace867978a2eb39216b51e5eb7
kevinbarra <98494916+kevinbarra@users.noreply.github.com>
2024-07-09 15:32:02 -0600

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,23 +1,20 @@
 module.exports = {
-    content: [
-        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    ],
-    images: {
-      domains: ['res.cloudinary.com'],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: "15px",
+      },
+      colors: {
+        accent: "#8a3ab9",
+        blackish: "#1b1b1b",
+      },
     },
-    theme: {
-        extend: {
-            container: {
-                center: true,
-                padding: "15px"
-            },
-            colors: {
-                accent: "#8a3ab9",
-                blackish: "#1b1b1b"
-            }
-        }
-    },
-    plugins: [],
-}
+  },
+  plugins: [],
+};
