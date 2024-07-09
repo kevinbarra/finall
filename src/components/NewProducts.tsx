23db4d1890a37deb08d29e61e8bd2e3a7ae637ba
kevinbarra <98494916+kevinbarra@users.noreply.github.com>
2024-07-09 16:02:23 -0600

    Update NewProducts.tsx

--- src/components/NewProducts.tsx
+++ src/components/NewProducts.tsx
@@ -37,9 +37,9 @@ const NewProducts: React.FC<NewProductsProps> = ({ products, category }) => {
                     {category}
                 </h2>
                 <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-2 sm:gap-4">
-                    {products.map((item) =>
+                    {products.map((item) => (
                         <ProductCard key={item.ID_product} product={item} onProductSelect={showProductDetails} />
-                    )}
+                    ))}
                 </div>
             </div>
             <ProductDetailsModal product={selectedProduct} onClose={closeProductDetails} />
